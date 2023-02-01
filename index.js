3db4b176490f36d877341669664b097da2e3ebd8
Fio25 <119542407+Fio25@users.noreply.github.com>
2023-02-01 10:36:05 +0100

    formulaire

---
+++ index.js
@@ -0,0 +1,16 @@
+// document.getElementById("email").addEventListener("blur", function(event) {
+
+// });
+
+// function verifAge (birthday) {
+//     const today = new Date();
+//     const birthDate = new Date(birthday);
+//     const age = today.getFullYear() - birthDate.getFullYear();
+//     const ageLimit = 13;
+//     if (age < ageLimit) {
+//         return false;
+//     }
+//     else {
+//         return true;
+//     }
+// }
